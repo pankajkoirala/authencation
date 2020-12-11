a23d50c6b39a4cc68f10065d9c24e0284dfa4aea
pankajkoirala <pankaj.koirala.77@gmail.com>
2020-12-11 20:40:47 +0545

    ok

--- src/container/route/privateRoute.js
+++ src/container/route/privateRoute.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Redirect, Route } from "react-router-dom";
 
 let PrivateRoute = ({ component: Component, ...rest }) => {
-  const [authorize, setAuthorize] = useState(false);
+  const [authorize, setAuthorize] = useState(true);
   return (
     <Route
       {...rest}
